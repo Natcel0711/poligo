80b24292c248c8e8afd7a0f35747064a4e44c3b7
Natcel0711 <natcelnieves@gmail.com>
2023-02-05 10:10:04 -0400

    adding graceful shutdown

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/Natcel0711/poligo/config"
@@ -17,6 +21,45 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+	PORT := "0.0.0.0:" + env.PORT
+	server := &http.Server{Addr: PORT, Handler: service(env)}
+	// Server run context
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
+
+	// Listen for syscall signals for process to interrupt/quit
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-sig
+
+		// Shutdown signal with grace period of 30 seconds
+		shutdownCtx, _ := context.WithTimeout(serverCtx, 30*time.Second)
+
+		go func() {
+			<-shutdownCtx.Done()
+			if shutdownCtx.Err() == context.DeadlineExceeded {
+				log.Fatal("graceful shutdown timed out.. forcing exit.")
+			}
+		}()
+
+		// Trigger graceful shutdown
+		err := server.Shutdown(shutdownCtx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		serverStopCtx()
+	}()
+	log.Printf("listening on %s", PORT)
+	err = server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+
+	// Wait for server context to be stopped
+	<-serverCtx.Done()
+}
+
+func service(env config.EnvVars) http.Handler {
 	db, err := storage.BootstrapMongo(env.MONGODB_URI, env.MONGODB_NAME, 10*time.Second)
 	if err != nil {
 		panic(err.Error())
@@ -29,6 +72,5 @@ func main() {
 	userStore := user.NewUserStorage(db)
 	userController := user.NewUserController(userStore)
 	user.AddUserRoutes(r, userController)
-	fmt.Printf("listening on %s", "0.0.0.0:"+env.PORT)
-	http.ListenAndServe("0.0.0.0:"+env.PORT, r)
+	return r
 }
